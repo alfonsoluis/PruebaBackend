99c104524767171121fa61f92778f1d5322c0b44
fonch <fonch@ALF_PC>
2024-04-20 00:55:24 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace PruebaBackend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Author { get; set; }
+        public string? Genre { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaBackend.Models;
+
+namespace PruebaBackend.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; } = null!;
+    }
+
+}
